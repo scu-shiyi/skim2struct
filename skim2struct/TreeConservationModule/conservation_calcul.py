7202bb18b4fcf974501a14090501d5d0e7139114
shishi <s783414516@gmail.com>
2025-09-17 17:41:17 +0800

    Update conservation_calcul.py
    Update Evo module call handling

--- skim2struct/TreeConservationModule/conservation_calcul.py
+++ skim2struct/TreeConservationModule/conservation_calcul.py
@@ -2,21 +2,19 @@
 
 import pandas as pd
 import ast
-from skim2struct.utils.EvoScoring import EvoScoring
+from skim2struct.utils.EvoScoring import position_entropy
 
 def compute_entropy_matrix(fasta_path: str, output_dir: str) -> str:
-    """
-    调用 EvoScoring 计算位置熵矩阵，返回 CSV 路径。
-    """
-    model = EvoScoring(fasta_path, output_dir)
-    return model.position_entropy()
+    return position_entropy(fasta_path, output_dir)
 
-def load_heatmap_data(heatmap_path: str, leaf_labels: list[str]) -> pd.DataFrame:
-    """
-    加载 CSV，排序并转换为数值矩阵。
-    """
+def load_heatmap_data(heatmap_path: str, leaf_labels: list[str], outgroups=None) -> pd.DataFrame:
     heatmap_df = pd.read_csv(heatmap_path, index_col=0, header=0)
     heatmap_df = heatmap_df.map(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    if outgroups:
+        for og in outgroups:
+            if og in heatmap_df.index:
+                heatmap_df = heatmap_df.drop(index=og)
+                
     ordered_heatmap_df = heatmap_df.reindex(leaf_labels)
 
     return ordered_heatmap_df
