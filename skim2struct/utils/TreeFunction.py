1a3fa39aa659e8667c93f6673b8b7aa72d7ff99f
shishi <s783414516@gmail.com>
2025-09-18 09:06:08 +0800

    Update TreeFunction.py

--- skim2struct/utils/TreeFunction.py
+++ skim2struct/utils/TreeFunction.py
@@ -3,18 +3,7 @@ import subprocess
 import os
 from pathlib import Path
 def build_tree(fasta_file, temp_tree_dir):
-    """
-    使用 IQ-TREE 构建进化树，返回 .treefile 路径。
-    """   
-    """ 
-    temp_tree_dir
-        ├── Gene1
-            ├── 
-            └── Gene1.treefile
-        ├── Gene2
-            ├── 
-            └── Gene1.treefile
-    """
+
     gene_name = Path(fasta_file).stem
     gene_outdir = os.path.join(temp_tree_dir, gene_name)
     os.makedirs(gene_outdir, exist_ok=True)
@@ -24,7 +13,7 @@ def build_tree(fasta_file, temp_tree_dir):
 
     if os.path.exists(final_tree_path):
         return final_tree_path
-    # 2. 用 iqtree 生成 ML 树
+
     iqtree_cmd = ["iqtree2", "-s", fasta_file, "-pre", tree_prefix]
     subprocess.run(iqtree_cmd, check=True)
 
@@ -32,10 +21,7 @@ def build_tree(fasta_file, temp_tree_dir):
 
 
 def load_tree(tree_path: str):
-    """
-    读取并 ladderize 进化树。
-    返回 (tree, depths, max_depth)
-    """
+
     tree = Phylo.read(tree_path, "newick")
     tree.ladderize()
     depths = tree.depths()
@@ -43,9 +29,7 @@ def load_tree(tree_path: str):
     return tree, depths, max_depth
 
 def compute_leaf_positions(tree) -> dict:
-    """
-    生成叶节点在画布中的垂直位置（y 坐标）。
-    """
+
     terminals = tree.get_terminals()
     leaf_positions = {term.name: idx for idx, term in enumerate(terminals)}
     return leaf_positions
